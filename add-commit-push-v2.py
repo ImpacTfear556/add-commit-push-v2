21b897a28d2a9a5856a761ada09d7b7e5adae303
ImpacTfear556 <angelofornero4@gmail.com>
2023-11-05 12:06:09 -0600

    Update files

--- add-commit-push-v2.py
+++ add-commit-push-v2.py
@@ -1,10 +1,29 @@
 import os
+import sys
+
 print("testing")
-quit
+numOfArgs = len(sys.argv)
+print("Total Arguments Passed:" , numOfArgs) 
+message = "Update files"
+if numOfArgs == 3:   
+  if sys.argv[1] == "-m":
+    print("Number of args = 3 and p2 =-m")
+    message = sys.argv[2]
+
+print(message)
+
+
 
 print("Add Commit Push")
 print("\ngit status")
 os.system("git status")
+
+print("Do you want to continue with add commit push? (y):")
+confirm = input()
+if confirm != "y":
+    print("canceling ", confirm)
+    quit()
+
 print("\ngit add -A")
 os.system("git add -A")
 print('\ngit commit -m "Update files"')
